3fa384606ab6c4d6ccf6ac9e3a249d2c29d01c76
Daniel Daley-Montgomery <AlexanderDM@live.com>
2016-09-26 08:59:00 -0400

    Meeting #2

---
+++ Assets/Actor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour {
+
+	// 0 - 5  : left shoulder, elbow, wrist, hip, knee, foot
+	// 6 - 11 : right ^
+	// 12 - 14: neck, spine, pelvis
+
+	private ActorJoint[] skeleton = new ActorJoint[15];
+	private int lIndex = 0; //Joints captured by left controller
+	private int rIndex = 6; //Joints captured by right controller
+	private int cIndex = 12; //central joints
+
+	SteamVR_TrackedController lvr;
+	SteamVR_TrackedController rvr;
+
+	// Use this for initialization
+	void Start () {
+
+		//identify right and left via device index
+
+		//attach button input to method calls
+		lvr.OnTriggerClicked += addLeft ();
+		rvr.OnTriggerClicked += addRight ();
+		lvr.OnPadClicked += addCenter ();
+		rvr.OnPadClicked += addCenter ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void addLeft () {
+		if (lIndex < 5) {
+			ActorJoint [lIndex] = new ActorJoint (lvr.transform.position);
+			lIndex++;
+		}
+	}
+
+	void addRight () {
+		if (lIndex < 11) {
+			ActorJoint [rIndex] = new ActorJoint (lvr.transform.position);
+			rIndex++;
+		}
+	}
+
+	void addCenter () {
+		if (lIndex < 14) {
+			ActorJoint [cIndex] = new ActorJoint (lvr.transform.position);
+			cIndex++;
+		}
+	}
+}
---
+++ Assets/ActorJoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorJoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public ActorJoint(Vector3 pos) {
+		this.transform.position = pos;
+	}
+}
