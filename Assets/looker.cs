87a309495adf59b22e438b1f122533f479eefbb3
Mohamad Moneimne <moneimne@seas.upenn.edu>
2016-12-11 17:24:47 -0500

    Possibly damaged commit?

---
+++ Assets/looker.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class looker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.transform.LookAt(new Vector3(0, 100, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(new Vector3(0, 100, 0));
+    }
+}
